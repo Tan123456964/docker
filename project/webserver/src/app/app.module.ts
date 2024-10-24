d8680eb4631016a00795f2b922b820be6f7023c5
Tan123456964 <tanmoydas957@gmail.com>
2024-10-24 12:00:00 -0400

    Added BrowserAnimationsModule

--- project/webserver/src/app/app.module.ts
+++ project/webserver/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({ 
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
     bootstrap: [AppComponent]
     
 })
